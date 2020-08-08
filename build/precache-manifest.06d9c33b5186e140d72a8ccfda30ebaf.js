642cf62dafeb09578005812beed5bb53a7bbfdf7
Alan <agounley@gmail.com>
2020-08-08 16:36:52 -0400

    added files

---
+++ build/precache-manifest.06d9c33b5186e140d72a8ccfda30ebaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b362967f98d3e68f9a494bbe74d0d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a95ef323055160bc735",
+    "url": "/static/css/main.cdfd6f49.chunk.css"
+  },
+  {
+    "revision": "de110133324e7c9f20af",
+    "url": "/static/js/2.c9338ff7.chunk.js"
+  },
+  {
+    "revision": "908678959397f2e04ac5f8f7afda467f",
+    "url": "/static/js/2.c9338ff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a95ef323055160bc735",
+    "url": "/static/js/main.6a9e7419.chunk.js"
+  },
+  {
+    "revision": "ecac48b033c6337c785d",
+    "url": "/static/js/runtime-main.94d669c2.js"
+  },
+  {
+    "revision": "f748e4acf2e8d231c08a93161117df43",
+    "url": "/static/media/Alan Gounley NEW resume 07-09-2020_word365.f748e4ac.pdf"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/resume_website/precache-manifest.e29269b0cc9bfb1aec97a4639b4d2174.js"
+  "/precache-manifest.06d9c33b5186e140d72a8ccfda30ebaf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/resume_website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
