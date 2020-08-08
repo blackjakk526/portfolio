8a4bb4d4fcf7ad9f978ec40da9f378a74291127b
Alan <agounley@gmail.com>
2020-08-08 16:44:35 -0400

    Updates

---
+++ precache-manifest.6a01d9411a5c81ae35886fe02aee03d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d18d0a845ab4cfd1ff2a3b9fad01518a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6a95ef323055160bc735",
+    "url": "/portfolio/static/css/main.cdfd6f49.chunk.css"
+  },
+  {
+    "revision": "de110133324e7c9f20af",
+    "url": "/portfolio/static/js/2.c9338ff7.chunk.js"
+  },
+  {
+    "revision": "908678959397f2e04ac5f8f7afda467f",
+    "url": "/portfolio/static/js/2.c9338ff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a95ef323055160bc735",
+    "url": "/portfolio/static/js/main.6a9e7419.chunk.js"
+  },
+  {
+    "revision": "77bc2915181200d0e7b4",
+    "url": "/portfolio/static/js/runtime-main.41450780.js"
+  },
+  {
+    "revision": "f748e4acf2e8d231c08a93161117df43",
+    "url": "/portfolio/static/media/Alan Gounley NEW resume 07-09-2020_word365.f748e4ac.pdf"
+  }
+]);
